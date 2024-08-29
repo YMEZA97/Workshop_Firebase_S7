12bec25d5193018fb34751ed76a8359163c50a44
YMEZA97 <yasalberto@hotmail.com>
2024-08-28 20:39:34 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# workshop_firebase_s7
+
+A new Flutter project.
+# Workshop_Firebase_S7
---
+++ android/app/src/main/kotlin/com/example/workshop_firebase_s7/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.workshop_firebase_s7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
